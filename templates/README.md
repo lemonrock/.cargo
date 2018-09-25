fe9b363dd47aedaa6483641142086ba7e760ae21
Raphael Cohn <raphael.cohn@stormmq.com>
2018-09-25 09:37:37 +0100

    Adjusting copyright and infill

--- templates/README.md
+++ templates/README.md
@@ -1,9 +1,4 @@
-[](This file is part of .cargo. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/.cargo/master/COPYRIGHT. No part of .cargo, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.)
-[](Copyright © 2016 The developers of .cargo. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/.cargo/master/COPYRIGHT.)
-
 # .cargo
-
-[![Clippy Linting Result](https://clippy.bashy.io/github/lemonrock/.cargo/master/badge.svg?style=plastic)](https://clippy.bashy.io/github/lemonrock/.cargo/master/log) [![](https://img.shields.io/badge/Code%20Style-rustfmt-brightgreen.svg?style=plastic)](https://github.com/rust-lang-nursery/rustfmt#configuring-rustfmt)
 
 [.cargo] is a rust crate.
 
--- templates/lib.rs
+++ templates/lib.rs
@@ -1,4 +1,16 @@
 // This file is part of .cargo. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/.cargo/master/COPYRIGHT. No part of .cargo, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
-// Copyright © 2016 The developers of .cargo. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/.cargo/master/COPYRIGHT.
+// Copyright © 2018 The developers of .cargo. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/.cargo/master/COPYRIGHT.
+
+
+#![allow(non_snake_case)]
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![deny(missing_docs)]
+#![deny(unreachable_patterns)]
+
+
+//! #.cargo
+//! 
+//! This is a rust library.
 
 
