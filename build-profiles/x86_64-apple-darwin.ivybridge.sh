a90bbd2fed3dfa72eb8e9ba658b7f53c07794435
Raphael James Cohn <raphael.cohn@stormmq.com>
2021-10-17 08:38:01 +0100

    In progress migration

--- build-profiles/x86_64-apple-darwin.ivybridge.sh
+++
@@ -1,8 +0,0 @@
-# This file is part of r-parser. It is subject to the license terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/r-parser/master/COPYRIGHT. No part of r-parser, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
-# Copyright © 2021 The developers of r-parser. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/r-parser/master/COPYRIGHT.
-
-
-# eg check, build or publish
-action="$1"
-target="$2"
-cpu_profile="$3"
